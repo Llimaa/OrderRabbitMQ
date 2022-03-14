1dc03b256e7a3a2f416a89f9d166c5f1e18d91c9
Marcos_Lima <limaa45@outlook.com>
2022-03-13 21:29:26 -0300

    implementando o consumo do evento

--- OrderRabbitMQ/Startup.cs
+++ OrderRabbitMQ/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OrderRabbitMQ.Consumers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +27,8 @@ namespace OrderRabbitMQ
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<RabbitMqConfigurate>(Configuration.GetSection("RabbitMqConfigurate"));
+            services.AddHostedService<ProcessOrderService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
